a40f23cb550350a9b34de6549ba8c567d6abe453
tinahung126 <tina126hung@gmail.com>
2021-05-19 22:28:41 +0800

    feat: create seeds data

--- models/seeds/shortenerSeeder.js
+++ models/seeds/shortenerSeeder.js
@@ -1,2 +1,17 @@
 const Url = require('../url');
 const db = require('../../config/mongoose');
+
+const data = [
+  { originUrl: 'https://www.google.com/', shortenUrl: 'ttgWc' },
+  { originUrl: 'https://tw.yahoo.com/', shortenUrl: 'P2Lhc' },
+];
+
+db.once('open', () => {
+  Url.create(data)
+    .then(() => {
+      console.log('Category created!');
+      return db.close();
+    })
+    .then(() => console.log('Database connection closed'))
+    .catch((error) => console.log(error));
+});
